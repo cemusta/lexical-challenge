811f0178b00ce2bafa1dc5ecd327c601483b2154
Cem Usta <nooneelse@gmail.com>
2020-04-20 19:13:54 +0200

    covered one edge case

--- src/services/lexicalService.js
+++ src/services/lexicalService.js
@@ -4,10 +4,13 @@ const validate = (input) => {
   assert.ok(input, 'input is missing')
 
   const wordCount = input.split(' ').length
-  assert.ok(wordCount < 100, 'too many words')
+  assert.ok(wordCount <= 100, 'too many words')
 
   const charCount = input.length
-  assert.ok(charCount < 1000, 'input too long')
+  assert.ok(charCount <= 1000, 'input too long')
+
+  const normalized = input.replace(/[.,/#!$%^&*;:{}=\-_`~()]/g, ' ').replace(/\s+/g, ' ').trim()
+  assert.ok(normalized.length > 0, 'input missing any word')
 }
 
 const getRatio = (input, nonLexical) => {
@@ -16,6 +19,7 @@ const getRatio = (input, nonLexical) => {
 
   // normalization: replace punctuation, remove extra spaces, trim and then lowercase
   const normalized = input.replace(/[.,/#!$%^&*;:{}=\-_`~()]/g, ' ').replace(/\s+/g, ' ').trim().toLowerCase()
+  // assert.ok(input.length > 1, 'input is missing, after normalization')
 
   // tokenize
   const words = normalized.split(' ')
--- src/services/lexicalService.test.js
+++ src/services/lexicalService.test.js
@@ -3,18 +3,55 @@ const nonLexical = require('../constants/nonLexical')
 
 const first = 'Kim loves going to the cinema'
 const second = 'Cem does not love going to the cinema'
+
 const multiSentence = `${first}. ${second}`
 
 describe('Testing complexity service', () => {
   describe('validate()', () => {
-    it('should return exception: input is missing', async () => {
+    const words = '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20'
+    const wordLimit = `${words} ${words} ${words} ${words} ${words}`
+    const chars = '1234567890abcde1234567890'
+    const twoHunderdChars = `${chars}${chars}${chars}${chars}${chars}${chars}${chars}${chars}`
+    const charLimit = `${twoHunderdChars}${twoHunderdChars}${twoHunderdChars}${twoHunderdChars}${twoHunderdChars}`
+
+    it('should accept normal input', async () => {
+      expect(() => lexicalService.validate(first))
+        .not.toThrow('input is missing')
+    })
+
+    it('should return not exception: with valid input', async () => {
       expect(() => lexicalService.validate())
         .toThrow('input is missing')
     })
 
-    it('should return not exception: with valid input', async () => {
-      expect(() => lexicalService.validate(first))
-        .not.toThrow('nonLexical is missing')
+    it('should accept long but within limit input', async () => {
+      expect(() => lexicalService.validate(wordLimit))
+        .not.toThrow('too many words')
+    })
+
+    it('should return not exception: too many words', async () => {
+      expect(() => lexicalService.validate(wordLimit + ' 101'))
+        .toThrow('too many words')
+    })
+
+    it('should accept long but within limit input - char', async () => {
+      expect(() => lexicalService.validate(charLimit))
+        .not.toThrow('input too long')
+    })
+
+    it('should return not exception: too many chars', async () => {
+      expect(() => lexicalService.validate(charLimit + 'X'))
+        .toThrow('input too long')
+    })
+
+    it('should return exception if only spaces', async () => {
+      expect(() => lexicalService.validate('     '))
+        .toThrow('input missing any word')
+    })
+
+    it('should return exception if only dots', async () => {
+      expect(() => lexicalService.validate('.........'))
+        .toThrow('input missing any word')
     })
   })
 
